3a3159bd61d3a848ee93a76a8c368f03201ab115
Hussain Tamboli <hussaintamboli18@gmail.com>
2014-12-10 18:38:06 +0530

    some generic naming

--- python/kombu/publisher.py
+++ python/kombu/publisher.py
@@ -6,21 +6,16 @@ import uuid
 import json
 
 userid, password, virtual_host, uri_prefix = 'celery_user', \
-                  'celery_pass', 'delivery_reports', 'amqp://'
+                  'celery_pass', '/', 'amqp://'
 # or you could directly pass
 # conn_settings = 'amqp://celery_user:celery_pass@localhost/delivery_reports'
 # to the Connection()
 
 queue_name, exchange, routing_key = 'celery', 'celery', 'celery'
-task_id, task_name = str(common.uuid()), 'CeleryWorker.PushToSalesforce'
+task_id, task_name = str(common.uuid()), 'CeleryWorker.Process'
 
 data = {
-    "smsId" : "3568084",
-    "status" : "DELIVERED",
-    "externalId" : "00590000002lBajAAE-451506f12265",
-    "accountId" : "80001837",
-    "sentStatus" : "success",
-    "responseId" : "00590000002lBajAAE-ef5dd6b1edd5"
+    "elem" : "data goes here"
 }
 payload = {
     "id" : task_id,
