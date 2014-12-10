4ac836a6eed3022a76f8e9f306dbc77777eed8c9
Hussain Tamboli <hussaintamboli18@gmail.com>
2014-12-10 18:36:53 +0530

    task data as args in the payload. additionally added some elements to the payload

--- python/kombu/publisher.py
+++ python/kombu/publisher.py
@@ -1,4 +1,7 @@
-from kombu import Connection
+from kombu import Connection, Producer, Queue
+from kombu import common
+from kombu.message import Message
+from kombu.simple import SimpleQueue
 import uuid
 import json
 
@@ -9,23 +12,38 @@ userid, password, virtual_host, uri_prefix = 'celery_user', \
 # to the Connection()
 
 queue_name, exchange, routing_key = 'celery', 'celery', 'celery'
-task_id, task_name = str(uuid.uuid1()), 'CeleryWorker.PushToSalesforce'
-payload = {
-    "smsId":"3568084",
-    "status":"DELIVERED",
-    "externalId":"00590000002lBajAAE-451506f12265",
-    "accountId":"80001837",
-    "sentStatus":"success",
-    "responseId":"00590000002lBajAAE-ef5dd6b1edd5"
+task_id, task_name = str(common.uuid()), 'CeleryWorker.PushToSalesforce'
+
+data = {
+    "smsId" : "3568084",
+    "status" : "DELIVERED",
+    "externalId" : "00590000002lBajAAE-451506f12265",
+    "accountId" : "80001837",
+    "sentStatus" : "success",
+    "responseId" : "00590000002lBajAAE-ef5dd6b1edd5"
 }
-message = json.dumps(payload)  #json payload
+payload = {
+    "id" : task_id,
+    "task" : task_name,
+    "args" : [data],
+    "retries" : 0
+}
+payload = json.dumps(payload)  #json payload
 
 with Connection(userid = userid, password = password, 
                 virtual_host = virtual_host, 
                 uri_prefix = uri_prefix) as conn:
-    simple_queue = conn.SimpleQueue(queue_name)
-    simple_queue.put(message)
-    print('sending task with task_id=%s having payload=%s' % (queue_name, message))
-    simple_queue.close()
+    channel = conn.channel()
+    queue = Queue(queue_name, exchange = exchange, routing_key = routing_key)
+    #message = Message(channel, body = payload)
+    producer = Producer(conn)
+    #message = Message(jsonpayload)
+    producer.publish(payload, routing_key = routing_key, exchange = exchange, 
+                     correlation_id = task_id, reply_to = queue_name)
+    #simple_queue = conn.SimpleQueue(queue_name)
+    #simple_queue.put(message)
+    print('sending task with task_id=%s having payload=%s' % (task_id, payload))
+    #simple_queue.close()
+    channel.close()
     conn.release()
 
